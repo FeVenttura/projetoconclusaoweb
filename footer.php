8f0f54f851b4bea0f2a83f768d7c9801b4433a8c
FeVenttura <fe.vntr@gmail.com>
2024-11-06 20:26:20 -0300

    mudança-logo

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@
         </li>
     </ul>
 
-    <p class="text-center text-body-secondary">© 2024 Empresa, Inc</p>
+    <p class="text-center text-body-secondary">© 2024 Voluntários do Conhecimento, inc</p>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
--- menu.php
+++ menu.php
@@ -1,7 +1,8 @@
-<!-- https://getbootstrap.com/docs/5.3/components/navbar/#nav -->
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
-        <a class="navbar-brand" href="#">Meu site - Logotipo</a>
+        <a class="navbar-brand" href="#">
+            <img src="assets/images/logo-jpg.jpg" alt="Logotipo" style="height: 100px;">
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
